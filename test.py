a27c8091dd3fb51fa74715a6b502499867de17e0
Oliver Parr <139624027+OPSPESH@users.noreply.github.com>
2023-10-27 14:09:04 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 from turtle import Screen
-import turtle, random
-def move(turtle):
-    xcor = random.randint(turtle.xcor()-50, tr)
-    turtle.goto()
-
-xcor = turtle.xcor()
-goto1 = int(xcor) + int(10)
-goto2 = int(xcor) - int(10)
-goto = random.randint(int(goto1),int(goto2))
-print(goto)
+import turtle, random, time
+turtle.speed(1)
+while True:
+    gotox = [
+        turtle.xcor() - 10,
+        turtle.xcor() + 10
+    ]
+    xgotox = random.randint(0,1)
+    gotoy = [
+        turtle.ycor() - 10,
+        turtle.ycor() + 10
+    ]
+    ygotoy = random.randint(0,1)
+    goto1 = [
+        gotox[xgotox],
+        gotoy[ygotoy]
+    ]
+    goto2 = random.randint(0,1)
+    print(gotox[xgotox])
+    print(gotoy[ygotoy])
+    turtle.goto(
